0b9acedeaa9e39ef830074113c6aaa5685ab1b48
Edward Kandrot <edward@prey4.us>
2012-05-21 18:35:47 -0700

    working version of 2.1

--- mus.js
+++ mus.js
@@ -1,8 +1,5 @@
 var prelude = function(expr) {
-    var s = new String(expr);
-    var x = "{ tag: 'seq',\n  left: { tag: 'note', pitch: 'd4', dur: 500 },\n  right: " + str(expr);
-    return x + "}"
-
+    return {tag:'seq', left: {tag: 'note', pitch: 'd4', dur: 500}, right: expr};
 };
 
 var melody1 = { tag: 'note', pitch: 'c4', dur: 250 };
@@ -19,3 +16,4 @@ var melody3 =
          right: { tag: 'note', pitch: 'c4', dur: 250 } } };
 
 console.log(prelude(melody1));
+console.log(prelude(melody2));
