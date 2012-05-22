546b31f011418be102314b6f208cc2d7f8cef362
Edward Kandrot <edward@prey4.us>
2012-05-21 18:49:05 -0700

    lession 2.2 completed

--- mus.js
+++ mus.js
@@ -1,19 +1,48 @@
-var prelude = function(expr) {
-    return {tag:'seq', left: {tag: 'note', pitch: 'd4', dur: 500}, right: expr};
-};
-
 var melody1 = { tag: 'note', pitch: 'c4', dur: 250 };
+
 var melody2 = 
     { tag: 'seq',
       left: { tag: 'note', pitch: 'd4', dur: 500 },
       right: { tag: 'note', pitch: 'c4', dur: 250 } };
+
 var melody3 = 
     { tag: 'seq',
-      left: { tag: 'note', pitch: 'd4', dur: 500 },
-      right:
+      left:
        { tag: 'seq',
          left: { tag: 'note', pitch: 'd4', dur: 500 },
-         right: { tag: 'note', pitch: 'c4', dur: 250 } } };
+         right: { tag: 'note', pitch: 'c4', dur: 500 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'b4', dur: 250 },
+         right: { tag: 'note', pitch: 'a4', dur: 250 } } };
 
-console.log(prelude(melody1));
-console.log(prelude(melody2));
+
+
+// Lesson 2.1
+
+var prelude = function(expr) {
+    return {tag:'seq', left: {tag: 'note', pitch: 'd4', dur: 500}, right: expr};
+};
+
+//console.log(prelude(melody1));
+//console.log(prelude(melody2));
+
+
+// Lesson 2.2
+
+var reverse = function(expr) {
+    if (expr.tag == 'note')
+        return expr;
+    if (expr.tag == 'seq')
+        return { tag:'seq',
+            left: reverse( expr.right ),
+            right: reverse( expr.left )};
+};
+
+//console.log(reverse(melody1));
+//console.log(reverse(melody2));
+//console.log(reverse(melody3));
+
+
+// Lesson 2.3
+
