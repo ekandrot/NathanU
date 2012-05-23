e87c2a82ec972a9008795cbac76f99e32b38db5b
Edward Kandrot <edward@prey4.us>
2012-05-23 09:18:45 -0700

    completed lesson 4.2

--- lesson4/evalScheem.js
+++ lesson4/evalScheem.js
@@ -20,3 +20,34 @@ var evalScheem = function (expr) {
 };
 
 
+// lesson 4.2
+
+var evalScheem = function (expr, env) {
+    // Numbers evaluate to themselves
+    if (typeof expr === 'number') {
+        return expr;
+    }
+    // Strings are variable references
+    if (typeof expr === 'string') {
+        return env[expr];
+    }
+    // Look at head of list for operation
+    switch (expr[0]) {
+        case '+':
+            return evalScheem(expr[1], env) +
+                   evalScheem(expr[2], env);
+        case '-':
+            return evalScheem(expr[1], env) -
+                   evalScheem(expr[2], env);
+        case '*':
+            return evalScheem(expr[1], env) *
+                   evalScheem(expr[2], env);
+        case '/':
+            return evalScheem(expr[1], env) /
+                   evalScheem(expr[2], env);
+    }
+};
+
+
+// lesson 4.3
+
