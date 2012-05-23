751fec66cf02f62ec8b08c05accabf5236b303e0
Edward Kandrot <edward@prey4.us>
2012-05-23 09:30:32 -0700

    completed lesson 4.3

--- lesson4/evalScheem.js
+++ lesson4/evalScheem.js
@@ -50,4 +50,27 @@ var evalScheem = function (expr, env) {
 
 
 // lesson 4.3
+// the spec says to return zero on defines, but I'd rather it return the value
+// "Never throw away work" - we calculated the value, might as well return it
+// but for this excerise, matching the spec is the way to go...
 
+var evalScheem = function (expr, env) {
+    // Numbers evaluate to themselves
+    if (typeof expr === 'number') {
+        return expr;
+    }
+    // Strings are variable references
+    if (typeof expr === 'string') {
+        return env[expr];
+    }
+    // Look at head of list for operation
+    switch (expr[0]) {
+        case '+':
+            return evalScheem(expr[1], env) +
+                   evalScheem(expr[2], env);
+        case 'set!':
+        case 'define':
+            env[expr[1]] = evalScheem(expr[2],env);
+            return 0;
+    }
+};
